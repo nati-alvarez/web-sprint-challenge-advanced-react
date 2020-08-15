a39bd82e88fcd220ed0cb1905f773307078d380b
Natividad Alvarez <tivialvarez@gmail.com>
2020-08-14 20:30:09 -0400

    created filter function for plants list'

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -6,7 +6,8 @@ export default class PlantList extends Component {
   constructor(){
     super();
     this.state = {
-      plants: []
+      plants: [],
+      query: ""
     }
   }
 
@@ -16,6 +17,7 @@ export default class PlantList extends Component {
   componentDidMount(){
     axios.get("http://localhost:3333/plants").then(({data})=>{
       this.setState({
+        ...this.state,
         plants: data.plantsData
       })
     }).catch(err=>{
@@ -23,32 +25,58 @@ export default class PlantList extends Component {
     })
   }
   
+  filterPlants = (e) =>{
+    e.preventDefault();
+    const queryRegex = new RegExp(this.state.query, "gi");
+    axios.get("http://localhost:3333/plants").then(({data})=>{
+      this.setState({
+        ...this.state,
+        plants: data.plantsData.filter(plant=>{
+          return plant.name.match(queryRegex);
+        })
+      })
+    }).catch(err=>{
+      console.log(err);
+    })
+  }
+
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
-      <main className="plant-list">
-        {this.state?.plants?.map((plant) => (
-          <div className="plant-card" key={plant.id}>
-            <img className="plant-image" src={plant.img} alt={plant.name} />
-            <div className="plant-details">
-              <h2 className="plant-name">{plant.name}</h2>
-              <p className="plant-scientific-name">{plant.scientificName}</p>
-              <p>{plant.description}</p>
-              <div className="plant-bottom-row">
-                <p>${plant.price}</p>
-                <p>☀️ {plant.light}</p>
-                <p>💦 {plant.watering}x/month</p>
+      <>
+        <form onSubmit={this.filterPlants} className="search-plants">
+          <input 
+            onChange={e => this.setState({...this.state, query: e.target.value})}
+            name="query"
+            placeholder="search for a plant" 
+            value={this.state.query}
+            type="text"/>
+          <button>Search</button>
+        </form>
+        <main className="plant-list">
+          {this.state?.plants?.map((plant) => (
+            <div className="plant-card" key={plant.id}>
+              <img className="plant-image" src={plant.img} alt={plant.name} />
+              <div className="plant-details">
+                <h2 className="plant-name">{plant.name}</h2>
+                <p className="plant-scientific-name">{plant.scientificName}</p>
+                <p>{plant.description}</p>
+                <div className="plant-bottom-row">
+                  <p>${plant.price}</p>
+                  <p>☀️ {plant.light}</p>
+                  <p>💦 {plant.watering}x/month</p>
+                </div>
+                <button
+                  className="plant-button"
+                  onClick={() => this.props.addToCart(plant)}
+                >
+                  Add to cart
+                </button>
               </div>
-              <button
-                className="plant-button"
-                onClick={() => this.props.addToCart(plant)}
-              >
-                Add to cart
-              </button>
             </div>
-          </div>
-        ))}
-      </main>
+          ))}
+        </main>
+      </>
     );
   }
 }
