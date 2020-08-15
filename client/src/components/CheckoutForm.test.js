e6629d4cb1616cd5f248da7ca10ea88f2d561be5
Natividad Alvarez <tivialvarez@gmail.com>
2020-08-14 20:10:12 -0400

    Wrote tests for CheckoutForm

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,41 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, act, fireEvent, getByText} from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const {getByText} = render(<CheckoutForm/>);
+    const formHeader = getByText("Checkout Form");
 
-test("form shows success message on submit with form details", () => {});
+    expect(formHeader).toBeInTheDocument();
+});
+
+test("form shows success message on submit with form details", () => {
+    const {getByTestId, getByLabelText, getByText} = render(<CheckoutForm/>);
+    const Firstname = getByLabelText("First Name:");
+    const Lastname = getByLabelText("Last Name:");
+    const Address = getByLabelText("Address:");
+    const City = getByLabelText("City:");
+    const State = getByLabelText("State:");
+    const Zip = getByLabelText("Zip:");
+    const CheckoutButton = getByText("Checkout");
+
+    act(()=>{
+        fireEvent.change(Firstname, {target: {value: "Nativdad"}});
+        fireEvent.change(Lastname, {target: {value: "Alvarez"}});
+        fireEvent.change(Address, {target: {value: "123 Place St"}});
+        fireEvent.change(City, {target:{value: "Somewhere"}});
+        fireEvent.change(State, {target:{value: "SC"}});
+        fireEvent.change(Zip, {target:{value: "12345"}});
+
+        fireEvent.click(CheckoutButton);
+    });
+
+    const successMessage = getByTestId("successMessage");
+
+    expect(successMessage).toBeInTheDocument();
+    expect(successMessage).toHaveTextContent(/Woo-hoo/g);
+
+    console.log(Firstname.value, Lastname.value)
+});
